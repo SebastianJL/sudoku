f1174b69a5cfbab5cdff354d085c8901394ffaba
SebastianJL <johannes.lade@yahoo.com>
2015-08-08 08:46:11 +0200

    First commit.

---
+++ sudoku_tests.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  7 12:24:43 2015
+
+@author: Johannes Lade
+"""
+
+from __future__ import division
+from sudoku_class import Sudoku
+
+
+#Reduction Tests
+################
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print 1
